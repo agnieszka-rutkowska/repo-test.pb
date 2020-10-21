685da33ff48c0bf70346b267dc9415b838895e0c
repo-test <macbook@MacBooks-MacBook.local>
2020-10-21 14:58:14 +0200

    liczby parzyste

---
+++ code_test.js
@@ -0,0 +1,12 @@
+// const a=2;
+// const b=3;
+//0
+const tab = [1,2,3,4,5,6,7,8,9,11,12,13,21,25];
+
+for(let i=0; i < tab.length; i++){
+    if(tab[i]%2=0){
+        console.log('jestem liczba parzysta ',tab[i]);
+    }
+}
+
+console.log(tab[9]);
